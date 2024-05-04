a33714e7fc660c39e85cbc483658a964e91b8c46
Natcha Ranchan <natcha.ran@ku.th>
2024-05-04 22:07:20 +0700

    preview

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SkyVista
-test readme
+## Description:
+This program aims to help user make airlines decision to avoid flight delays
+
+
+## Data source:
+from Kaggle
+
+## Installation requirement:
+## *need to use python 3.12 version
+1. numpy~=1.26.4  
+2. pandas~=2.2.2  
+3. matplotlib~=3.9.0rc2
+4. pillow~=10.3.0
+
+
+## How to run
+python main.py
--- model.py
+++ model.py
@@ -1,11 +1,8 @@
 
-# for model and controller
+# for controller
 from database import Data
 from app import UI,UI1,UI2
 import numpy as np
-
-class Model:
-    pass
 
 class Controller:
     def __init__(self,data):
@@ -14,10 +11,12 @@ class Controller:
         # self.ui1 = UI1(self,self.data.df)
         # self.ui2 = None
         # self.state = 1
+        # page 2 first
         self.state = 2
         self.ui2 = UI2(self, self.data.df)
 
     def switch_mode(self, mode):
+        # change application state and switch the mode
         print('state', self.state)
         if mode == 'Explore' and self.state == 2:
             self.ui2.destroy()
@@ -33,10 +32,12 @@ class Controller:
 
     def run(self):
         # self.ui1.run()
+        #page 2 first
         self.ui2.run()
 
-    def df_groupby(self,groupby_attr,mean_attr, df):
-        return self.data.mean_groupby_df(groupby_attr,mean_attr, df)
+    def df_groupby(self, groupby_attr, mean_attr, df):
+        # return dataframe of groupby atr and find mean
+        return self.data.mean_groupby_df(groupby_attr, mean_attr, df)
 
 
     def get_unique_airlines(self,df):
@@ -46,7 +47,8 @@ class Controller:
         return self.data.all_origin()
 
     def get_all_dest(self,orig):
-        return self.data.all_destination(orig)
+        self.ui2.pick_dest['values'] = self.data.all_destination(orig)
+        # return self.data.all_destination(orig)
 
     def filter_origin_and_dest(self, orig, dest):
         self.ui2.temp_data = self.data.filtered_attributes_dist(orig, dest)
@@ -59,30 +61,54 @@ class Controller:
 
     def stat_columns(self):
         columns_atr = ["STATISTICS",'ARRIVAL_DELAY', 'AIR_TIME', 'DISTANCE', 'ELAPSED_TIME']
-        stat = ['Mean', 'Std', 'Min', '25', '50', '75', 'Max']
-        label = ['Statistics','Arrival delay', 'Air time', 'Distance','Elapsed time']
+        stat = ['mean', 'std', 'min', 'q1', 'q2', 'q3', 'max']
+        label = ['Statistics', 'Arrival delay', 'Air time', 'Distance','Elapsed time']
         return columns_atr, stat, label
 
-    def tree_view_data(self, stat):
-        data = {
-            # "Mean": [4.494781, 118.202505, 800.959041, 145.739040],
-            # "Std": [60.054968, 74.476176, 573.256670, 76.236987],
-            # "Min": [-56.000000, 32.000000, 184.000000,9.000000],
-            # "25%": [-15.000000, 57.000000, 280.000000, 90.000000],
-            # "50%": [-6.000000, 103.000000, 666.000000, 129.000000]
-            "ARRIVAL_DELAY": [4.494781, 60.054968, -56, -15, -6, 7, 1201],
-            "AIR_TIME": [118.202505, 74.476176, 32, 57, 103, 160, 423],
-            "DISTANCE": [800.959041, 573.256670, 184, 280, 666, 1242, 2611],
-            "ELAPSED_TIME": [145.739040, 76.236987, 49, 90, 129, 185.75, 447],
+    def tree_view_data(self, stat, df):
+        function = {
+            'mean': lambda df: round(df.mean(),2).tolist(),
+            'std': lambda df: round(df.std(), 2).tolist(),
+            'min': lambda df: df.min().tolist(),
+            'q1': lambda df: round(df.quantile(0.25),2).tolist(),
+            'q2': lambda df: round(df.quantile(0.50),2).tolist(),
+            'q3': lambda df: round(df.quantile(0.75),2).tolist(),
+            'max': lambda df: df.max().tolist()
         }
-        # print(data)
-        # print(self.data.df.loc[:1000,col].describe())
-        # print(self.data.df[0:1000])
-        # for i,df in zip(stat, self.data.df):
-        #     print(i,df.ARRIVAL_DELAY.mean())
+        data = {
+        #     # "mean": [6.6, 158.6, 925.0, 179.4]
+        #     # "std": [19.58, 12.6, 0.0, 17.34],
+        #     # "min": [-15.0, 141.0, 925.0, 158.0],
+        #     # "q1": [-2.0, 151.0, 925.0, 166.0],
+        #     # "q2": [-1.0, 163.0, 925.0, 182.0],
+        #     # "q3" : [15.0, 165.0, 925.0, 191.0],
+        #     # "max" : [36.0, 173.0, 925.0, 200.0]
+        #
+        #
+        #     # "ARRIVAL_DELAY": [4.494781, 60.054968, -56, -15, -6, 7, 1201],
+        #     # "AIR_TIME": [118.202505, 74.476176, 32, 57, 103, 160, 423],
+        #     # "DISTANCE": [800.959041, 573.256670, 184, 280, 666, 1242, 2611],
+        #     # "ELAPSED_TIME": [145.739040, 76.236987, 49, 90, 129, 185.75, 447],
+        }
+        # print(df.head())
         tree_view = []
-        for i, c in zip(stat, range(len(stat))):
-            tree_view.append((i, data["ARRIVAL_DELAY"][c], data["AIR_TIME"][c], data["DISTANCE"][c], data["ELAPSED_TIME"][c]))
+        for row in stat:
+            lst = function[row](df)
+            data[row] = lst
+            # tree_view.append((row, lst))
+        # print(7777,data)
+        # for row in range(len(col)):
+        # function['mean'](my_df)
+
+
+
+        for index, c in zip(stat, range(len(stat))):
+            temp = []
+            temp.append(index)
+            for val in data[index]:
+                temp.append(val)
+            tree_view.append(temp)
+            # tree_view.append((index, data[index][c], data[index][c], data[index][c], data[index][c]))
         return tree_view
 
     def get_airline_data(self, airline,ui_num=1):
@@ -132,19 +158,50 @@ class Controller:
 if __name__ == '__main__':
     data = Data()
     c = Controller(data)
-    stat = ['Mean', 'Std', 'Min', '25', '50', '75', 'Max']
+    stat = ['mean', 'std', 'min', 'q1', 'q2', 'q3', 'max']
     col = ['ARRIVAL_DELAY', 'AIR_TIME', 'DISTANCE', 'ELAPSED_TIME']
-    x = c.tree_view_data()
-    # for i in x:
-    #     print(i)
-    print(x)
+
     """
     ARRIVAL_DELAY
     AIR_TIME
     DISTANCE
     ELAPSED_TIME
     """
+    """
+    """
     print('-------')
+    filtered = data.df[(data.df.ORIGIN_AIRPORT == 'CHS') & (data.df.DESTINATION_AIRPORT == 'IAH')]
+    my_df = filtered.loc[:,col].head()
+    tree_view = c.tree_view_data(stat, my_df)
+    print(43434,tree_view)
+    print('+++++')
+    print(my_df.mean().tolist()) # Mean
+    print(round(my_df.std(),2).tolist())  # std
+    print(my_df.min().tolist())  # min
+    print(my_df.quantile(0.25).tolist())  # 25
+    print(my_df.quantile(0.5).tolist()) # 50
+    print(my_df.quantile(0.75).tolist()) # 75
+    print(my_df.max().tolist()) # max
+
+
+
+    # function = {
+    #             'mean': lambda df: df.mean().tolist(),
+    #             'std': lambda df: round(df.std(), 2).tolist(),
+    #             'min': lambda df: df.min().tolist(),
+    #             'q1': lambda df: df.quantile(0.25).tolist(),
+    #             'q2': lambda df: df.quantile(0.50).tolist(),
+    #             'q3': lambda df: df.quantile(0.75).tolist(),
+    #             'max': lambda df: df.max().tolist()
+    #             }
+    # print(888888, function['mean'](my_df))
+    # _mean = lambda df: df.mean().tolist()
+    # _std = lambda df: df.std().tolist()
+    # _min = lambda df: df.min().tolist()
+    # _25 = lambda df: df.quantile(0.25).tolist()
+    # _50 = lambda df: df.quantile(0.50).tolist()
+    # _75 = lambda df: df.quantile(0.75).tolist()
+    # _max = lambda df: df.max().tolist()
 
     # for c in range(len(stat)):
     # for i,c in zip(stat,len(stat):
